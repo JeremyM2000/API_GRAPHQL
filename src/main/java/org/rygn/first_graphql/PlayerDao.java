936593462ad9b59abed71df4a36896750136a0ef
JeremyM2000 <jeremy.meme@hotmail.fr>
2024-03-10 17:50:41 +0100

    Traitement getPlayerById / postOnePlayer / deleteAllPlayer / deleteOnePlayerById / putOnePlayer

--- src/main/java/org/rygn/first_graphql/PlayerDao.java
+++ src/main/java/org/rygn/first_graphql/PlayerDao.java
@@ -1,6 +1,7 @@
 package org.rygn.first_graphql;
 
 import java.util.List;
+import java.util.Objects;
 
 public class PlayerDao {
 
@@ -13,5 +14,41 @@ public class PlayerDao {
     public List<Player> getAllPlayer()
     {
         return this.players;
+    }
+
+    public Player getPlayerById(String id)
+    {
+        return this.players.stream().filter(player -> player.id.equals(id)).findFirst().orElse(null);
+    }
+
+    public Player postOnePlayer(Player player)
+    {
+        this.players.add(player);
+        return player;
+    }
+
+    public List<Player> deleteAllPlayer()
+    {
+        this.players.clear();
+        return this.players;
+    }
+
+    public List<Player> deleteOnePlayerById(String id)
+    {
+        this.players.removeIf(player -> Objects.equals(player.id, id));
+        return this.players;
+    }
+
+    public Player putOnePlayer(Player player)
+    {
+        for(Player p : this.players) {
+            if(p.id.equals(player.id))
+            {
+                this.players.set(this.players.indexOf(p), player);
+                break;
+            }
+        }
+
+        return player;
     }
 }
